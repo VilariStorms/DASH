cefc417c6d46b7af21c640873110cb6eb16ff2b5
glass-paramedic <glass-paramedic@protonail.com>
2023-07-17 23:27:40 +0100

    im a bit retarded

--- source/app.d
+++ source/app.d
@@ -1,4 +1,13 @@
+// DASH - D Programming Language Shell
+// Written by: XDRC
+// License: WTWYEDTL
+
+
 import std.stdio;
+import std.string;
+import std.conv;
+import std.file;
+
 
 void init_shell()
 {
@@ -12,9 +21,10 @@ void init_shell()
 
 void main() {
     init_shell();
+    string input;
     while (true) {
         write("$ ");
-        string input = readln();
+        readf("%s\n", &input);
         if (input == "help") {
             writeln("help - print this message");
             writeln("exit - exit the shell");
