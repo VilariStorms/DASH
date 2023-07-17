f6013fadb2fd2d4dafc6d022d5304b3a55d30346
Deltara <50772027+Deltara3@users.noreply.github.com>
2023-07-17 17:28:22 -0400

    Initialized project

---
+++ source/app.d
@@ -0,0 +1,26 @@
+
+import std.stdio;
+void init_shell()
+{
+    writeln("Welcome to DASH!");
+    writeln("Type 'help' to get started.");
+    writeln("please insert cash or select payment card");
+    writeln("You oww £79");
+    writeln();
+    writeln();
+}
+void main() {
+    init_shell();
+    while (true) {
+      write("$ ");
+        string input = readln();
+        if (input == "help") {
+            writeln("help - print this message");
+            writeln("exit - exit the shell");
+        } else if (input == "exit") {
+            break;
+        } else {
+            writeln("Unknown command: ", input);
+        }
+    }
+}
