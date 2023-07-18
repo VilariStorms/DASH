b7180a5d4aec58c2d0594b45c1e5065a042ed6f6
Deltara <50772027+Deltara3@users.noreply.github.com>
2023-07-18 14:58:43 -0400

    D sucks

--- source/shell/parser.d
+++ source/shell/parser.d
@@ -1,21 +1,54 @@
 import std.stdio;
 import std.process;
 import std.array;
+import std.file;
 import builtin;
 
 int parse_command(string input) {
-    string[] args = input.split();
-    int exit_code;
-    // check if builtin    
-    if (builtin_handler(args) == 0) { return 0; }
+    if (input == "") {
+        return 0;
+    } else {
+        string[] cmds = input.split("|");
+        string[][] args;
+        
+        foreach (cmd; cmds) { args ~= cmd.split(); }
 
-    // check if executable
-    try {
-        auto pid = spawnProcess(args);
-        exit_code = pid.wait();
-    } catch (ProcessException e) {
-        writefln("dash: command: %s, not found!", args);
-        return 1;
-    }  
-    return exit_code;
+        int exit_code;
+        File prev_out = File.tmpfile();
+        File in_buf = File.tmpfile();
+        string buf;
+
+        // check if builtin
+        foreach (i; 0..args.length) {
+            if (builtin_handler(args[i]) == 0) {
+                return 0;
+            } else {
+                // check if executable
+                try {
+                    if (args.length == 1) {
+                        auto pid = spawnProcess(args[i]);
+                        exit_code = pid.wait();
+                    } else {
+                        if (i == 0) {
+                            auto pid = spawnProcess(args[i], stdin, prev_out, stderr, null, Config.retainStdout);
+                            pid.wait();
+                            buf = prev_out.readln();
+                            writefln("%s", buf);
+                        } else if (i != 0 && i != args.length) {
+                            auto pid = spawnProcess(args[i], in_buf, prev_out, stderr, null, Config.retainStdout | Config.retainStdin);
+                            exit_code = pid.wait();
+                        } else {
+                            auto pid = spawnProcess(args[i], in_buf, stdout, stderr, null, Config.retainStdin);
+                            exit_code = pid.wait();
+                        }
+                    }
+                } catch (ProcessException e) {
+                    writefln("dash: command: %s, not found!", args[i][0]);
+                    return 1;
+                }  
+            }
+        }
+
+        return exit_code;
+    }
 }
